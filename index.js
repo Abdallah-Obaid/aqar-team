eb692cce729672043c1130033089b6000b9351d3
Abdallah <eng.abdallahobaid@gmail.com>
2021-02-05 18:15:47 +0200

    new db

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const mongoose = require('mongoose');
 const server = require('./src/server');
 require('dotenv').config();
-const MONGODB_URL = "mongodb://aaaa:aaaa@cluster0-shard-00-00.0igfj.mongodb.net:27017,cluster0-shard-00-01.0igfj.mongodb.net:27017,cluster0-shard-00-02.0igfj.mongodb.net:27017/aaaa?ssl=true&replicaSet=atlas-56dqcx-shard-0&authSource=admin&retryWrites=true&w=majority";
+// const MONGODB_URL = "mongodb://aaaa:aaaa@cluster0-shard-00-00.0igfj.mongodb.net:27017,cluster0-shard-00-01.0igfj.mongodb.net:27017,cluster0-shard-00-02.0igfj.mongodb.net:27017/aaaa?ssl=true&replicaSet=atlas-56dqcx-shard-0&authSource=admin&retryWrites=true&w=majority";
+const MONGODB_URL = "mongodb+srv://123:123@cluster0.gkzyb.mongodb.net/aqar?retryWrites=true&w=majority";
 const mongooseOptions = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
